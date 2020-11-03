caf0e43a11a632759bdfeee404193d0cc5c8c04f
jakemeout <jacobosity@gmail.com>
2020-11-03 15:12:35 -0500

    trees search

---
+++ Trees/twitter.js
@@ -0,0 +1,36 @@
+//calculate the number of distinct groupings that you can make via group and people.
+
+
+// people = 8
+// group = 4
+// output = 5 
+// [1,1,1,5]
+// [1,1,2,4]
+// [1,2,2,3]
+// [1,1,3,3]
+// [2,2,2,2]
+   0 1 2 3 4 5 6 7
+0  5 4 3 3
+1  1 2 2
+2  1 1 2
+3  1 1 1
+
+
+p7 g3
+
+5,1,1
+4,2,1
+3,3,1
+3,2,2
+
+
+p10 g4
+
+7,1,1,1
+
+
+
+function solution(people,group){
+    for
+
+}
