ba8922f3d05c066e9ee7decc68b9af380b6c32bc
jakemeout <jacobosity@gmail.com>
2020-12-09 10:54:08 -0500

    spaces

--- Search/linearSearch.js
+++ Search/linearSearch.js
@@ -1,10 +1,10 @@
-
-
 const linearSearch = (arr, val) => {
-    for(let i = 0; i < arr.length;i++) {
-        if(arr[i] === val){
-            return i 
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
     }
-    return -1
-}
+  }
+  return -1;
+};
+
+console.log(linearSearch([2, 4, 123, 53, 5, 4], 53));
