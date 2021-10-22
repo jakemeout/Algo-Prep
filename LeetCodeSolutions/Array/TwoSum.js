15567bf03f52bbcd99543e0aa56294014e58767f
jakemeout <jacobosity@gmail.com>
2021-10-22 14:35:38 -0400

    twoSum solution

---
+++ LeetCodeSolutions/Array/TwoSum.js
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/two-sum/
+ * Easy
+ *
+ * Strategy:
+ *   - As we move through the array subtract the target from the element in the array.
+ *   - Create a hashmap where you check if the difference is in the hashmap
+ *      if it is, then return the value at the given key in the hashmap (the index of the diff el)
+ *      and the el you are currently iterating over.
+ *   - Otherwise, return an empty array when the loop is done, meaning none of the elements add upto
+ *      the target
+ */
+
+const twoSum = (nums, target) => {
+  const numMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (diff in numMap) {
+      return [numMap[diff], i];
+    }
+    numMap[nums[i]] = i;
+  }
+  return [];
+};
+
+// const nums = [2,7,11,15];
+// const target = 9;
+// console.log(twoSum(nums,target))
