1346930dc038c2b53740dedc16eef267404ea680
jakemeout <jacobosity@gmail.com>
2021-11-16 15:55:38 -0500

    add function and insert updated

--- LeetCodeSolutions/buildAbinaryTree.js
+++ LeetCodeSolutions/buildAbinaryTree.js
@@ -33,14 +33,42 @@ class BST {
       }
     }
   }
+
+  add(value) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(value);
+      return;
+    } else {
+      const searchTree = (node) => {
+        if (value < node.value) {
+          if (node.left === null) {
+            node.left = new Node(value);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (value > node.value) {
+          if (node.right === null) {
+            node.right = new Node(value);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
+    }
+  }
 }
 
 const b = new BST();
-b.insert(8);
-b.insert(7);
-b.insert(10);
-b.insert(5);
-b.insert(4);
-b.insert(14);
-console.log(JSON.stringify(b))
-
+b.add(8);
+b.add(7);
+b.add(10);
+b.add(5);
+b.add(4);
+b.add(14);
+console.log(b);
