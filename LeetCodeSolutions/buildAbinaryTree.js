99a16053d81248e6137ec95aeb445007c79bcd2e
jakemeout <jacobosity@gmail.com>
2021-11-16 15:18:54 -0500

    build BST

--- LeetCodeSolutions/buildAbinaryTree.js
+++ LeetCodeSolutions/buildAbinaryTree.js
@@ -1,18 +1,46 @@
-
-
-class BTree {
-
-    constructor(value, left=null, right=null){
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-
-    insert(value){
-        if(value > this.value){
-            b
-        }
-    }
-
-
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
 }
+
+class BST {
+  constructor(root = new Node(null)) {
+    this.root = root;
+  }
+
+  insert(value) {
+    let currNode = this.root;
+    while (currNode) {
+      if (value < currNode.value) {
+        if (currNode.left === null) {
+          currNode.left = new Node(value);
+          currNode = null;
+        } else {
+          currNode = currNode.left;
+        }
+      } else if (value > currNode.value) {
+        if (currNode.right === null) {
+          currNode.right = new Node(value);
+          currNode = null;
+        } else {
+          currNode = currNode.right;
+        }
+      } else {
+        throw new Error("Value already in BST");
+      }
+    }
+  }
+}
+
+const b = new BST();
+b.insert(8);
+b.insert(7);
+b.insert(10);
+b.insert(5);
+b.insert(4);
+b.insert(14);
+console.log(JSON.stringify(b))
+
