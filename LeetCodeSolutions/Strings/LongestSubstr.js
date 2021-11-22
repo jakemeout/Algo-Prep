e7751b884fbc5dff133813f74af416ca86703821
jakemeout <jacobosity@gmail.com>
2021-11-22 14:42:36 -0500

    using sliding window

--- LeetCodeSolutions/Strings/LongestSubstr.js
+++ LeetCodeSolutions/Strings/LongestSubstr.js
@@ -5,6 +5,8 @@
  * Given a string s, find the length of the longest substring without repeating characters.
  *
  * Input: s = "abcabcbb"
+ *              ^
+ *              ^
  * Output: 3
  * Explanation: The answer is "abc", with the length of 3.
  *
@@ -14,19 +16,29 @@
  * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  *
  * Approach:
- *   - while looping over the array, look at the element a step ahead, if the element is different, add the element to a new string
- *   -  check to see if the element is in the string before putting it in the string.
+ *   - Sliding windows. Set both pointers to the first element and expand the window based on the condition of non repeating characters
+ *     find the longest window. 
+ *   - Using a Set we can create a unique set of non repeating characters and check the set for the character,
+ *     if it doesn't have the character add it to the set, then check the size, and get the max size of the window between i & J
+ *     if the letter already exists increment i to keep moving through the str and find the next set of unique non repeating characters. 
+ *   
  */
 
 const lengthOfLongestSubstring = (s) => {
-  let str = [];
+  let i = 0;
+  let j = 0;
+  let hashSet = new Set();
+  let maxWindow = 0;
 
-  for (let i = 1; i < s.length - 1; i++) {
-    if (s[i] !== s[i + 1] && !str.includes(s[i])) {
-      str.push(s[i]);
-    } else if (!str.includes(s[i])) {
-      str.push(s[s.length - 1]);
+  while (j < s.length) {
+    if (!hashSet.has(s[j])) {
+      hashSet.add(s[j]);
+      maxWindow = Math.max(hashSet.size, maxWindow);
+      j++;
+    } else {
+      hashSet.delete(s[i]);
+      i++;
     }
   }
-  return str.length;
+  return maxWindow;
 };
