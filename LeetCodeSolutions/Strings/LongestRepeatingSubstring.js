905d8a0a5e374eeec58f8c233ed6cb936d2d379c
jakemeout <jacobosity@gmail.com>
2021-11-23 12:49:41 -0500

    added longestrepeatingsubstring

---
+++ LeetCodeSolutions/Strings/LongestRepeatingSubstring.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maxPower = (s) => {
+  let i = 0;
+  let j = 0;
+  let maxRep = 0;
+  let count = 0;
+
+  while (j < s.length) {
+    if (s[j] === s[i]) {
+      count += 1;
+      maxRep = Math.max(maxRep, count);
+      j++;
+    } else {
+      count = 0;
+      i++;
+    }
+  }
+  return maxRep;
+};
+
+s = "abbcccddddeeeeedcba";
+console.log(JSON.stringify(maxPower(s)));
+
+/* Approach:
+ *  - sliding window of longest repeating character
+ *  s = "abbcccddddeeeeedcba"
+ *         i
+ *.        j
+ */
